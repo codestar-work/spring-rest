b6cc1b0fc6458f31795f295524191b8baa225bf4
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2015-06-07 00:18:47 +0700

    Simply the Best

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,16 +1,13 @@
 import org.springframework.boot.*;
-import org.springframework.stereotype.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 
 @RestController @EnableAutoConfiguration
-public class Example {
-
+class Example {
 	@RequestMapping("/")
 	String home() {
 		return "Hello World!";
 	}
-	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Example.class, args);
 	}
