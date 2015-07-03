7cd3482138e2e7f0068718b866a6fa6ebad44c32
Kookiat Suetrong <beer@Kookiats-MacBook-Air.local>
2015-07-04 01:30:41 +0700

    Update

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,6 +1,7 @@
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
+import java.util.*;
 
 @RestController @EnableAutoConfiguration
 class Example {
@@ -8,7 +9,24 @@ class Example {
 	String home() {
 		return "Hello World!";
 	}
+	
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Example.class, args);
+	}
+	
+	@RequestMapping("/student")
+	Student student() {
+		Student s = new Student();
+		s.name = "Harry";
+		s.height = 175;
+		return s;
+	}
+}
+
+class Student {
+	String name;
+	int height;
+	public String getName() {
+		return name;
 	}
 }
