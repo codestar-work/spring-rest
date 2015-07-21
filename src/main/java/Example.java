2ebc815c770b21ecc31262082d432b521ec46a2a
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2015-07-21 20:02:48 +0700

    Update

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -4,16 +4,26 @@ import org.springframework.web.bind.annotation.*;
 import java.util.*;
 
 @RestController @EnableAutoConfiguration
-class Example {
+public class Example {
 	@RequestMapping("/")
 	String home() {
 		return "Hello World!";
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Example.class, args);
 	}
-	
+
+	@RequestMapping()
+	String defaultMethod() {
+		return "Default Method";
+	}
+
+	@RequestMapping("/say/hello/{name}")
+	String hello(@PathVariable String name) {
+		return "Hello " + name;
+	}
+
 	@RequestMapping("/student")
 	Student student() {
 		Student s = new Student();
